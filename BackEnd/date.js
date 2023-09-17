3ca2412dbae1d2499254f190699eb73ee3dd0995
assyiha-ahmed <assiyaahmed75@gmail.com>
2023-09-17 18:32:03 +0300

    sms

--- BackEnd/date.js
+++ BackEnd/date.js
@@ -12,12 +12,14 @@ for(let i=0; i < donorsData.length; i++){
    let name = donorsData[i].name;
    let phone = donorsData[i].contactNumber;
 
+   console.log(phone);
+
     dateData(date,today,name,phone);
 
 }
 
 function dateData(date,today,name,phone){
-  
+
     let day = new Date(date).getDate();
     let month = (new Date(date).getMonth()+1) *30;
     let year =(new Date(date).getFullYear())*365;
@@ -36,6 +38,7 @@ function dateData(date,today,name,phone){
     if (dayDifference>=3){
         let message =`Dear ${name},\n\nI hope this email finds you well.\n\nI'm writing to you today with a heavy heart. It has been ${dayDifference} days since you registered to donate an organ, and we are still waiting for you to come to the hospital.\n\nWe know that you are a busy person, but we urge you to please make time to donate your organ. There are many patients who are waiting for a life-saving transplant, and some of them are in very critical condition.\n\nWe recently met a young woman named Sarah who is waiting for a kidney transplant. She is only 25 years old, but she has been on dialysis for the past 5 years. She is tired of being hooked up to a machine every day, and she just wants to live a normal life.\n\nWe also met a little boy named Michael who is waiting for a heart transplant. He is only 5 years old, and he has been in and out of the hospital his whole life. He is so tired of being sick, and he just wants to be able to play with his friends.\n\nThese are just two of the many patients who are waiting for a life-saving organ transplant. Your donation could save their life.\n\nWe know that this is a big decision, but we believe in you. You are a kind and compassionate person, and we know that you want to help others.\n\nPlease, make the time to come to the hospital and donate your organ. You could save a life.\n\nThank you for your consideration.\n\nSincerely,\n\n[Your name]`;
         console.log(message)
+
 
     sendSingleMessage(phone, message);
     }
@@ -61,4 +64,4 @@ function dateData(date,today,name,phone){
 //     });
 //     }
 
-export default sendSingleMessage;
+// export default sendSingleMessage;
--- BackEnd/nodemailer.js
+++ BackEnd/nodemailer.js
@@ -1,41 +1,26 @@
 import nodemailer from 'nodemailer';
-import fs from 'fs';
-
-let hospital = fs.readFileSync("allowedHospitals.txt","utf8");
-hospital = JSON.parse(hospital)
-console.log(typeof(hospital))
-
-for(let i=0;i<hospital.length;i++){
-  let  emailAddress = hospital[i].EmailAddress;
-   let password = hospital[i].password;
-  let  userName = hospital[i].userName;
-
-  console.log(emailAddress)
 
 const transporter = nodemailer.createTransport({
     service: "hotmail",
     auth: {
         user:"fenettnugusu@gmail.com",
-        pass: "fenet2127"
+        pass: "12345organ54321"
     }
 });
 
 
 
-const option = {
+const options = {
     from: "fenettnugusu@gmail.com",
-    to: emailAddress,
-    subject: "you are great",
-    text: `password:${password} userName: ${userName}`
+    to: "assiyaahmed75@gmail.com",
+    subject: "you are great!",
+    text: "wow!"
 };
 
-transporter.sendMail(option, function(err,info){
+transporter.sendMail(options, function(err,info){
     if(err){
         console.log(err);
         return;
     }
     console.log("sent:"+info.response);
 })
-
-}
- 
